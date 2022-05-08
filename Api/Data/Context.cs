2c9e280ebfcf034da3ff8dd138c268c5e3259770
OldSinner <antylifepatia@gmail.com>
2022-05-08 19:57:07 +0200

    FrameWork Add

--- Api/Data/Context.cs
+++ Api/Data/Context.cs
@@ -1,9 +1,10 @@
 using Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data
 {
-    public class Context : DbContext
+    public class Context : IdentityDbContext<AppUser>
     {
         public DbSet<TestModel> TestModel { get; set; }
         public Context(DbContextOptions<Context> opt) : base(opt)
--- Api/Program.cs
+++ Api/Program.cs
@@ -2,7 +2,10 @@
 
 //Add Configuration 
 using Api.Data;
+using Api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var config = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
@@ -13,6 +16,23 @@ var builder = WebApplication.CreateBuilder(args);
 //Database -----------------------------------------
 builder.Services.AddDbContext<Context>(x => x.UseMySql(config.GetValue<string>("localConnectionString"), new MySqlServerVersion(new Version(8, 0, 21))));
 //End Database
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    // Password settings.
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;
+});
 //Cors 
 builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
 {
