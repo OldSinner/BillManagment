7caa648445eb2be9030c08894a6a192af3a1099a
OldSinner <antylifepatia@gmail.com>
2022-10-13 22:52:27 +0200

    Small Fixtuer

--- front/src/components/layout/Layout.jsx
+++ front/src/components/layout/Layout.jsx
@@ -1,19 +1,18 @@
-import Sidebar from "../sidebar/Sidebar";
-import "./Layout.css";
-import Addinvoice from "../addinvoice/Addinvoice";
-import AuthService from "../Auth/AuthService";
-import Redirect from "../Util/Redirect";
+import Sidebar from '../sidebar/Sidebar'
+import './Layout.css'
+import Addinvoice from '../addinvoice/Addinvoice'
+import AuthService from '../Auth/AuthService'
+import Redirect from '../Util/Redirect'
 export const Layout = ({ children }) => {
-  const _auth = AuthService;
-  const isLogged = _auth.isLogged();
+  const _auth = AuthService
+  const isLogged = _auth.isLogged()
   if (!isLogged) {
-    return <Redirect path={"/login"} />;
+    return <Redirect path={'/login'} />
   }
   return (
     <div className="layoutWrapper">
       <Sidebar></Sidebar>
-      <Addinvoice></Addinvoice>
-      <div className="">{children}</div>
+      {children}
     </div>
-  );
-};
+  )
+}
